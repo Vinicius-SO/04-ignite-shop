e19596117891a16faa6d26c2944773875dc6bba1
unknown <vhisoares39@gmail.com>
2023-12-09 00:24:11 -0300

    Add: Stitches config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,22 @@
+import { styled } from "../styles"
+
+const Button = styled('button',{
+  backgroundColor: '$green500',
+  borderRadius: 4,
+  border: 0,
+  padding:'4px 8px',
+
+  span:{
+    fontWeight:"bold"
+  },
+  '&:hover': {
+    filter:'brightness(0.8)',
+    cursor: 'pointer'
+  }
+})
+
 export default function Home() {
   return (
-    <h1>hello world</h1>
-    )
+    <Button><span>Click </span> Para Enviar </Button>  
+  )
 }
