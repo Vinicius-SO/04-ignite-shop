401957246dad042cce23cbb2a382b83265964bd9
unknown <vhisoares39@gmail.com>
2023-12-12 00:18:43 -0300

    Add: Add GetStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,18 @@
 import Image from 'next/image'
-import { HomeContainer, Product } from "../styles/pages/home"
+import { GetStaticProps } from 'next'
+
+import { stripe } from '../lib/stripe'
+import Stripe from 'stripe'
 
 import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
 
+import { HomeContainer, Product } from "../styles/pages/home"
 
 import camiseta1 from '../assets/Shirt/Type6.png'
 import camiseta2 from '../assets/Shirt/Type7.png'
 import camiseta3 from '../assets/Shirt/Type8.png'
-import { stripe } from '../lib/stripe'
-import { GetServerSideProps } from 'next'
-import Stripe from 'stripe'
+
 
 type HomeProps = {
   products: {
@@ -47,7 +49,7 @@ export default function Home({products}:HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async()=>{
+export const getStaticProps: GetStaticProps = async()=>{
   const response = await stripe.products.list({
     expand:['data.default_price']
   })
@@ -67,6 +69,7 @@ export const getServerSideProps: GetServerSideProps = async()=>{
   return{
     props:{
       products,
-    }
+    },
+    revalidate: 60 * 60 * 2  //2 hours ,
   }
 }
