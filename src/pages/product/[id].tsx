fff8c61c4fc123f72a670c3dcd5fc7d0a14913c8
unknown <vhisoares39@gmail.com>
2023-12-13 13:40:27 -0300

    Add: Product Page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,25 @@
+import { ImageContainer, ProductContainer, ProductDetails } from '@/src/styles/pages/product'
+import Image from 'next/image'
 import {useRouter} from 'next/router'
 
 export default function Product(){
     const { query } = useRouter()
     
     return(
-        <h1>Product: "id"":"{query.id}"</h1>
+        <ProductContainer>
+            <ImageContainer>
+                {/* <Image/> */}
+            </ImageContainer>
+            <ProductDetails>
+                <h1>Camiseta X</h1>
+                <span>R$ 79,90</span>
+
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis praesentium adipisci vero at quo temporibus dolore! Pariatur dolorum consequuntur, inventore quis reiciendis corporis. Repellendus amet optio numquam consequuntur dolores ea.</p>
+
+                <button>
+                    Comprar agora
+                </button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
