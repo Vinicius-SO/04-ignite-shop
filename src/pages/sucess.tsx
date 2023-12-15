af50efe29d27b3199fc61c642c12f700485c1058
unknown <vhisoares39@gmail.com>
2023-12-14 22:28:30 -0300

    Fix: Passing wrong sessionId

--- src/pages/sucess.tsx
+++ src/pages/sucess.tsx
@@ -32,21 +32,38 @@ export default function Sucess ({customerName,product}: SucessProps){
 }
 
 export const getServerSideProps :GetServerSideProps = async({query, params})=> {
+    if(!query.session_id){
+        return{
+            redirect:{
+                destination:'/',
+                permanent: false
+            }
+        }
+    }
     const sessionId =String(query.session_id)
 
-    const session = await stripe.checkout.sessions.retrieve(sessionId, {
-        expand:['line_items', 'line_items.data.price.product']
-    })
-
-    const customerName = session.customer_details.name
-    const product = session.line_items.data[0].price.product as Stripe.Product
-
-    return{
-        props:{
-            customerName,
-            product:{
-                name:product.name,
-                imageUrl: product.images[0]
+    try{
+        const session = await stripe.checkout.sessions.retrieve(sessionId, {
+            expand:['line_items', 'line_items.data.price.product']
+        })
+    
+        const customerName = session.customer_details.name
+        const product = session.line_items.data[0].price.product as Stripe.Product
+    
+        return{
+            props:{
+                customerName,
+                product:{
+                    name:product.name,
+                    imageUrl: product.images[0]
+                }
+            }
+        }
+    }catch(err){
+        return{
+            redirect: {
+                destination: '/',
+                permanent: false
             }
         }
     }
